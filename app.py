f3647e34f2d2875792894b0f3431cc92c40caf70
Matt Linder <mholmeslinder@gmail.com>
2021-01-23 12:57:23 -0800

    app and inference

--- app.py
+++ app.py
@@ -6,15 +6,20 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def rock_paper_scissor():
-    # Write the GET Method to get the index file
-
-    # Write the POST Method to post the results file
-
+    #POST Method to post the results file
+    if request.method == 'POST':
+    # POST Method to post the results file
     # Read file from upload
-    # Get category of prediction
-    # Plot the category
-    # Render the result template
-    pass
+        img = request.files['file']
+        # Get category of prediction
+        image_category = get_category(img)
+        # Plot the category
+        plot_category(img)
+        # Render the result template
+        return render_template('result.html', category=image_category)
+    # For GET requests, load the index file
+    return render_template('index.html')
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
--- inference.py
+++ inference.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+import os
 
 
 def get_category(img):
@@ -13,7 +14,70 @@ def get_category(img):
     Returns:
         [str]: Prediction
     """
-    pass
+
+    # # load the image
+    # image = mpimg.imread(img)
+
+    # # load the labels
+    # with open('labels.txt', 'r') as f:
+    #     labels = {i: line.strip() for i, line in enumerate(f.readlines())}
+
+    # # load the model
+    # interpreter = tf.lite.Interpreter(model_path='static/model/converted_model.tflite')
+    # interpreter.allocate_tensors()
+
+    # # get model input details and resize image
+    # input_details = interpreter.get_input_details()
+    # iw = input_details[0]['shape'][2]
+    # ih = input_details[0]['shape'][1]
+    # image = image.resize((iw, ih)).convert(mode='RGB')
+
+    # # set model input and invoke
+    # input_data = np.array(image).reshape((ih, iw, 3))[None, :, :, :]
+    # interpreter.set_tensor(input_details[0]['index'], input_data)
+    # interpreter.invoke()
+
+    # # read output and dequantize
+    # output_details = interpreter.get_output_details()[0]
+    # output = np.squeeze(interpreter.get_tensor(output_details['index']))
+    # if output_details['dtype'] == np.uint8:
+    #     scale, zero_point = output_details['quantization']
+    #     output = scale * (output - zero_point)
+
+    # # return the top label and its score
+    # ordered = np.argpartition(-output, 1)
+    # label_i = ordered[0]
+    # result = {'label': labels[label_i], 'score': output[label_i]}
+
+    # return result
+    # Read an image from a file into a numpy array
+    img = mpimg.imread(img)
+    # Convert to float32
+    img = tf.cast(img, tf.float32)
+    # Expand img dimensions from (224, 224, 3) to (1, 224, 224, 3) for set_tensor method call
+    img = np.expand_dims(img, axis=0)
+
+    tflite_model_file = 'static/model/converted_model.tflite'
+
+    with open(tflite_model_file, 'rb') as fid:
+        tflite_model = fid.read()
+
+    interpreter = tf.lite.Interpreter(model_content=tflite_model)
+    interpreter.allocate_tensors()
+
+    input_index = interpreter.get_input_details()[0]["index"]
+    output_index = interpreter.get_output_details()[0]["index"]
+    
+    prediction = []
+    interpreter.set_tensor(input_index, img)
+    interpreter.invoke()
+    prediction.append(interpreter.get_tensor(output_index))
+
+    predicted_label = np.argmax(prediction)
+    class_names = ['rock', 'paper', 'scissors']
+    
+    return class_names[predicted_label]
+
 
 
 def plot_category(img):
@@ -22,4 +86,17 @@ def plot_category(img):
     Args:
         img [jpg]: image file
     """
-    pass
+
+    img = mpimg.imread(img)
+    # Remove the plotting ticks
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+    plt.imshow(img, cmap=plt.cm.binary)
+    # matplotlib would not overwrite previous image, so using this work around
+    # https://stackoverflow.com/questions/49039581/matplotlib-savefig-will-not-overwrite-old-files
+    strFile = 'static/images/output.png'
+    if os.path.isfile(strFile):
+        os.remove(strFile)
+    # Save the image with the file name that result.html is using as its img src
+    plt.savefig(strFile)
